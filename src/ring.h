c1d08ac665d35be31f30be0b57164e2b93a646e3
Michał Szwejk <mszwejk1@gmail.com>
2026-01-15 02:18:35 +0100

    feat: implement token forwarding

--- src/ring.h
+++ src/ring.h
@@ -1,26 +1,33 @@
-#include <stdbool.h>
-#include <stdint.h>
+#ifndef RING_H
+#define RING_H
 
-#define TOKEN_DATA_SIZE 256
+#include "route.h"
+#include <stdbool.h>
+
+#define MAX_TOKEN_SIZE 256
 
 typedef struct Token {
-    char* sender;
-    char* reciever;
-    char* data;
+    char data[MAX_TOKEN_SIZE];
+    char sender[MAX_NODE_NAME_SIZE];
+    char reciever[MAX_NODE_NAME_SIZE];
     bool is_empty;
 } token_t;
 
-typedef struct Route {
-    char* node_name;
-    uint16_t port;
-} route_t;
 
-typedef struct RouteConfig {
-    route_t current;
-    route_t prev;
-    route_t next;
-} route_config_t;
+typedef struct Sockets {
+    int unicast_socket;
+    // int broadcast_socket;
+    // int cli_socket;
+} sockets_t;
 
 
-int fill_config(route_config_t* config);
-int setup_ring(route_config_t* config);
+int setup_unicast(route_t* config);
+int setup_broadcast_socket();
+int setup_cli_socket();
+
+
+int forward_token(route_t* config, int unicast_sock, token_t* token);
+
+int event_loop(route_config_t* config, sockets_t* sockets);
+
+#endif
