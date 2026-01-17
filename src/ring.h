f61a293359f90dd46fff47787b9714c95d17718d
KamilMarszalek <kamilmar553@gmail.com>
2026-01-18 00:33:24 +0100

    feat: add ring_state_t structure for managing ring state

--- src/ring.h
+++ src/ring.h
@@ -9,7 +9,20 @@ typedef struct Descriptors {
     int cli_fd;
 } descriptors_t;
 
-int ring_initalize(route_config_t* config, descriptors_t* descriptors);
-int ring_run(route_config_t* config, descriptors_t* descriptors);
+typedef struct {
+    route_config_t config;
+    join_state_t join_state;
+
+    token_t token_in;
+    int have_token;
+
+    token_t cli_pending;
+    int have_cli_pending;
+
+    struct timeval forward_at;
+} ring_state_t;
+
+int ring_initialize(route_config_t* config, descriptors_t* descriptors);
+int ring_run(route_config_t config, descriptors_t descriptors);
 
 #endif
